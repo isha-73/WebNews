b357c2d92d69545134aaebed6f33c7e642098294
Isha Bule <ishabule753@gmail.com>
2023-06-29 00:39:10 +0530

    categories

--- webnews/src/App.js
+++ webnews/src/App.js
@@ -16,38 +16,38 @@ import { Switch } from 'react-router-dom';
 function App() {
   return (
     <ChakraProvider>
-    <div className="App">
-      {/* <Router> */}
-      <header className="App-header">
-       <NavBar marginTop={'02px'}  />
-      </header>
-      {/* <div className='news'> */}
-     
-      {/* <Switch> */}
-          {/* <Route path="/business"> */}
-            <News category='business'/>
-          {/* </Route>
-          <Route path="/entertainment">
-            <News category='entertainment'/>
-          </Route>
-          <Route path="/general">
-            <News category='general'/>
-          </Route>
-          <Route path="/health">
-            <News category='health'/>
-          </Route>
-          <Route path="/science">
-            <News category='science'/>
-          </Route>
-          <Route path="/technology">
-            <News category='technology'/>
-          </Route> */}
-         
-     
-{/* </Switch>
+      <div className="App">
+        <Router>
+          <header className="App-header">
+            <NavBar marginTop={'02px'} />
+          </header>
+          {/* <div className='news'> */}
+
+          <Switch>
+            <Route path="/business">
+              <News category='business' />
+            </Route>
+            <Route path="/entertainment">
+              <News category='entertainment' />
+            </Route>
+            <Route path="/general">
+              <News category='general' />
+            </Route>
+            <Route path="/health">
+              <News category='health' />
+            </Route>
+            <Route path="/science">
+              <News category='science' />
+            </Route>
+            <Route path="/technology">
+              <News category='technology' />
+            </Route>
+
+
+          </Switch>
+          {/* </div> */}
+        </Router>
       </div>
-      </Router> */}
-    </div>
     </ChakraProvider>
   );
 }
